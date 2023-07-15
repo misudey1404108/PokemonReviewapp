3949215597651b4eb50bba8d5c682e748cb30b62
misu.dey <misudey1404108@gmail.com>
2023-07-15 20:35:19 +0600

    initial commit

---
+++ PokemanReviewApp/Models/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PokemanReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public ICollection<Review>? Reviews { get; set; }
+        public ICollection<PokemonOwner>? PokemonOwners { get; set; }
+        public ICollection<PokemonCategory>? PokemonCategories { get; set; }
+
+       
+
+    }
+}
